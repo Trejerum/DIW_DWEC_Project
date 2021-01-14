ae55ab2d85016dddd62454a8b375530376263744
Trejerum <dicorral28@gmail.con>
2021-01-14 11:57:16 +0100

    Comments added to the indexScript for each function

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,19 +1,34 @@
-function rememberGame() {
+/**
+ * Redirects the user to the memory game
+ */
+function memoryGame() {
   location.href = "html/memoryGame.html";
 }
 
+/**
+ * Redirects the user to the jumping game
+ */
 function jumpingGame() {
   location.href = "html/jumpingGame.html";
 }
 
+/**
+ * Redirects the user to the reflexes game
+ */
 function reflexGame() {
   location.href = "html/reflexGame.html";
 }
 
+/**
+ * Redirects the user to the snake game
+ */
 function snakeGame() {
   location.href = "html/snakeGame.html";
 }
 
+/**
+ * Redirects the user to the shooting game
+ */
 function shootingGame() {
   location.href =  "html/shootingGame.html";
 }
